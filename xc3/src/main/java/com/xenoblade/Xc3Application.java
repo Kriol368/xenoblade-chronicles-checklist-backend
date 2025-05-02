cce411f55ee78a378d5eb249b5a5eeb24f389065
Kriol368 <kriol368@gmail.com>
2025-05-02 16:54:37 +0200

    Corrected dockerfiles

---
+++ xc3/src/main/java/com/xenoblade/Xc3Application.java
@@ -0,0 +1,13 @@
+package com.xenoblade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xc3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Xc3Application.class, args);
+    }
+
+}
